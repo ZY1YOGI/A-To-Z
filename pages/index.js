737eefe494af2ea9f0a210cad41f459fc2f78511
ZY1YOGI <ZY1YOGI@GMAIL.COM>
2023-02-11 14:35:48 +0200

    mange files index space V-1.9.9

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import WhyUs from "@/components/WhyUs";
 import Head from "next/head";
+
 export default function Home() {
   return (
     <>
