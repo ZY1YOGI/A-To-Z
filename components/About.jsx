9c1c3bd20cc22110aa2af9858d5bbf8aef21c55c
ZY1YOGI <ZY1YOGI@GMAIL.COM>
2023-02-11 10:49:39 +0200

    edit clients and add services and mange V-06.9

--- components/About.jsx
+++ components/About.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import { MdDoneAll } from "react-icons/md"
 
 export default function About() {
   return (
-    <section id="about" className="container mx-auto my-3 border border-fuchsia-600" >
-      <h1 className="text-3xl font-bold py-2 dark:text-white decoration-slice underline underline-offset-[5px] decoration-fuchsia-700" data-aos="fade-up">ABOUT US</h1>
+    <section id="about" className="container mx-auto my-16 overflow-hidden" >
+      <h1 className="text-3xl text-center font-bold py-2 dark:text-white decoration-slice underline underline-offset-[5px] decoration-fuchsia-700" data-aos="fade-up" data-aos-delay="200">ABOUT US</h1>
+
+      <div className="flex space-x-8 max-sm:block max-sm:space-y-8 max-sm:space-x-3">
+        <div className="p-3 w-3/6 max-sm:w-full">
+          <h2 className="dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt  tempor  ut labore et doloreExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia ulpa qui officia deserunt mollit anim id est laborum. deserunt mollit anim id est laborum.</h2>
+          <ul className="mt-5">
+            <li className="flex space-x-1 transition-all duration-300 hover:-translate-y-1 text-gray-700 dark:text-gray-400 hover:font-bold hover:text-fuchsia-600 dark:hover:text-white cursor-default" data-aos="fade-left" data-aos-delay="150"><MdDoneAll size={25} /> <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat</p> </li>
+            <li className="flex space-x-1 transition-all duration-300 hover:-translate-y-1 text-gray-700 dark:text-gray-400 hover:font-bold hover:text-fuchsia-600 dark:hover:text-white cursor-default" data-aos="fade-left" data-aos-delay="350"><MdDoneAll size={25} /> <p>Duis aute irure dolor in reprehenderit in voluptate velit</p> </li>
+            <li className="flex space-x-1 transition-all duration-300 hover:-translate-y-1 text-gray-700 dark:text-gray-400 hover:font-bold hover:text-fuchsia-600 dark:hover:text-white cursor-default" data-aos="fade-left" data-aos-delay="500"><MdDoneAll size={25} /> <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat</p> </li>
+          </ul>
+        </div>
+
+        <div className="p-3 w-3/6 max-sm:w-[93%] max-sm:px-0">
+          <p className="dark:text-white">Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </p>
+          <button className="dark:hover:bg-transparent hover:bg-gray-900  text-fuchsia-500 dark:bg-gray-800 dark:text-fuchsia-700 px-5 py-2 outline outline-2  outline-offset-1 rounded-3xl mt-5">
+            Learn More
+          </button>
+        </div>
+      </div>
+
     </section>
   )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,8 +7,7 @@ export default function Hero() {
       <div className="container mx-auto flex items-center max-sm:flex-col">
         <div className="max-sm:order-2 text-center">
           <h1 className="font-bold text-3xl max-sm:text-xl dark:text-gray-100">A-Z {'>'}  The best solutions for your Business</h1>
-          <p className="max-sm:text-xs dark:text-gray-400 capitalize">We are a company that manufactures web software, desktop software, mobile app software, and all accounting
-            software</p>
+          <p className="max-sm:text-xs dark:text-gray-400 capitalize">We are a company that manufactures web software, desktop software, mobile app software, and all accounting software</p>
           <div className="flex space-x-5 justify-center mt-3">
             <button type="button" className="dark:hover:bg-transparent hover:bg-gray-900 bg-gray-700 text-fuchsia-500 dark:bg-white dark:text-fuchsia-700 px-5 py-2 outline outline-2  outline-offset-1 rounded-3xl">Get Started</button>
             <button type="button" className="flex items-center text-fuchsia-500 dark:text-white px-5 py-1 hover:bg-gray-50 dark:hover:bg-white dark:hover:text-gray-900  transition-al  outline outline-2  outline-offset-1 rounded-3xl"><BsFillPlayFill size={35} />Watch Video</button>
