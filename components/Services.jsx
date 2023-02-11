898f1d056764ead6db97658af13bae9cf19d83ce
ZY1YOGI <ZY1YOGI@GMAIL.COM>
2023-02-11 23:48:58 +0200

    mange file V-2.5.1

--- components/Services.jsx
+++ components/Services.jsx
@@ -6,6 +6,7 @@ import { RiUserSettingsLine } from "react-icons/ri"
 export default function Services() {
   return (
     <section id="services" className="container mx-auto my-16">
+
       <div className="text-center">
         <h1 className="text-3xl font-bold py-2 dark:text-white decoration-slice underline underline-offset-[5px] decoration-fuchsia-700" data-aos="fade-up" data-aos-delay="200">SERVICES</h1>
         <p className="text-gray-700 dark:text-gray-300 capitalize" data-aos="fade-down">We always care about our services, whether it is software, hardware, hardware or software maintenance.</p>
