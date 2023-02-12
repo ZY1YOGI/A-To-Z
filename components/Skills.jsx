2743985d3fc36658b17133d5891eeaf20dabf2fd
ZY1YOGI <ZY1YOGI@GMAIL.COM>
2023-02-12 19:41:54 +0200

    add section skills V-2.5.4

---
+++ components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BsFillArrowDownCircleFill, BsFillArrowUpCircleFill } from 'react-icons/bs'
+
+export default function Skills() {
+  return (
+    <section id="skills" className="my-20 bg-white dark:bg-gray-800  border-x-[5px] border-red-600">
+
+    </section>
+  )
+}
+
+
+
