70ce433fbace949e731138fe99429dab5f087a93
ZY1YOGI <ZY1YOGI@GMAIL.COM>
2023-02-20 19:19:15 +0200

    Mange Api-Products V-7.9.6

--- pages/category/[category].js
+++ pages/category/[category].js
@@ -41,12 +41,12 @@ export default function ({ category }) {
 
           {
             category.products.map((product, index) => (
-              <div className="text-white transition-all duration-300 cursor-pointer hover:-translate-y-6 hover:text-fuchsia-600 dark:hover:text-red-700 hover:font-bold" key={index}>
+              <div className="text-gray-900 transition-all duration-300 cursor-pointer hover:-translate-y-6 hover:text-fuchsia-600 dark:hover:text-red-700 hover:font-bold" key={index}>
                 <div className="overflow-hidden border shadow-md bg-gray-50 dark:bg-gray-800 shadow-fuchsia-700 rounded-xl border-fuchsia-600" data-aos="fade-up" data-aos-delay="250">
                   <Image src={`https://2796-41-43-95-76.eu.ngrok.io/${product.img}`} width={350} height={350}  className="h-[210px] w-full" alt="Image" />
                   <div className="p-5">
                     <h1 className="text-xl text-center">{product.name}</h1>
-                    <h2><span className="dark:text-white">Model: </span>{product.model}</h2>
+                    <h2><span className="text-gray-900 dark:text-white">Model: </span>{product.model}</h2>
                     <p className="font-normal text-gray-700 dark:text-gray-300">{product.info}</p>
                     <a href="/" type="button" className="outline outline-2 outline-offset-4 outline-red-700 my-5 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Show Product.</a>
                   </div>
